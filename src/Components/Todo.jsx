240368cf109f3eccd0530f5b7438b412e56f3878
codewitgabi <gabrielmicheal497@gmail.com>
2023-04-26 01:56:00 +0100

    Added delete todo functionality

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,11 +1,32 @@
+import { useContext } from "react";
+import { TodoContext } from "../Context/TodoContext";
 
 
 function Todo({ todo }) {
+  const { todos, setTodos } = useContext(TodoContext);
+
+  const handleDelete = () => {
+    const newTodos = todos.filter(td => td.id !== todo.id);
+
+    setTodos(newTodos);
+  };
+
   return (
     <div class="todo">
-      <legend class="title">{ todo.title }</legend>
-      <small class="date">{ todo.date }</small>
-      <p class="body">{ todo.body }</p>
+      <div>
+        <legend class="title">{ todo.title }</legend>
+        <small class="date">{ todo.date }</small>
+        <p class="body">{ todo.body }</p>
+      </div>
+      <div>
+        <button className="upd-btn">Update</button>
+        <button
+          className="del-btn"
+          onClick={ handleDelete }
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 }
